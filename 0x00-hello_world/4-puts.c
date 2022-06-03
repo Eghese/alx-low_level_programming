c4f81b1b6eac8c71b37c55d32f87c340e9729531
Eghese <eghese123@gmail.com>
2022-06-03 08:21:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+ * Description: main - Prints outs a sring 
+ * Return:0 if success 
 
 int main (void)
 {
 	puts("\"programming is like building a multilingual puzzle");
+	
 	return(0);
 	
 }		
