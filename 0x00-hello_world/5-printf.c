d21f4316e7c16aca7e17252429312b31e36e61a9
Eghese <eghese123@gmail.com>
2022-06-03 10:14:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include<studio.h>
+#include <studio>
 /**
- * Description: main - prints out a string.
+ * Description: main - prints out a string
  *
- * Return 0 if success
+ * Return: 0 if success
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
