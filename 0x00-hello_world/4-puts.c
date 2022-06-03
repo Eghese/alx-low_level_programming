79e29c77fbd6cef06c735f81138811df9ef7f7ff
Eghese <eghese123@gmail.com>
2022-06-03 09:11:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Description: main - Prints outs a sring. 
- * Return:0 if success.
- */ 
-int main (void)
+ * Description: main - Prints outs a sring.
+ * Return:0 if success
+ */
+int main(void)
 {
-	puts ("\"programming is like building a multilingual puzzle");
-	return(0);
-}		
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
