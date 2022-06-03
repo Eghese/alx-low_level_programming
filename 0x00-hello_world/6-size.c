a926c49fa81ac8efd672f7af229565481d8bd53d
Eghese <eghese123@gmail.com>
2022-06-03 14:46:54 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 /**
+ * Description: main - print out the string
  *
+ * Return: 0 if succcess
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a char: %d byte(s)\n", sizeof(int));
+	printf("size of a char: %d byte(s)\n", sizeof(long int));
+	printf("size of a char: %d byte(s)\n", sizeof(long long int));
+	printf("size of a char: %d byte(s)\n", sizeof(float));
+	return (0);
+}
+
+ 
