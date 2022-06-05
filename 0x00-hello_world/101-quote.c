ae859837d065df63935baf641deeacecd1237f85
Eghese <eghese123@gmail.com>
2022-06-05 13:54:30 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
  /**
-  * Description: main - printing output of a string.
+  * main - prints value to the stderr
   *
   * Return: 1 if success
   */
@@ -9,7 +9,7 @@ int main(void)
 {
 	int start = 1;
 	int end = start + 58;
-char msg[60] = " and that of art is useful\" - Dora Korpar, 2015-10-19\n";
+char msg[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(msg, end, start, stderr);
 	return (1);
