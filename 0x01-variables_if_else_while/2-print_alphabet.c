7f8f5c981e43a6b9f1f214849f4949d553c6a651
Eghese <eghese123@gmail.com>
2022-06-05 16:27:19 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Description: main - printing the output of an alphabet.
+ *
+ * putchar: return an output
+ */
+int main()
+{
+	int ch;
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
