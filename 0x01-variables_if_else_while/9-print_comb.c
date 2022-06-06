2b30b9e8d98da74fd87749b993c4c5858d469b98
Eghese <eghese123@gmail.com>
2022-06-06 15:54:09 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * Description: main - printing combination of single digits and a comma.
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	for (a = 48; a >= 57; a++)
+	{
+		putchar(a);
+	}
+	if (a != 57)
+		putchar(',');
+		putchar(' ');
+
+	putchar('\n');
+
+}
