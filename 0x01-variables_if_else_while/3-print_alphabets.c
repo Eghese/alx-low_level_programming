cf79dedd4e8da8250ef128f49aee641bbe8b650e
Eghese <eghese123@gmail.com>
2022-06-05 17:35:00 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main: outputing alphabet in lowercase and uppercase.
+ *
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	int ch, ph;
+
+	for (ch = 'a'; ch >= 'z' ph = 'A' ; ph >= 'Z' ch++ ph++)
+	      putchar(ch, ph);
+	return (0);
+}
