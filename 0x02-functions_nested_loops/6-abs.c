27194783ed7fb77b5ddd3f6771ecf8f5b1b905ee
Eghese <eghese123@gmail.com>
2022-06-08 15:12:12 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - printing the signs of a number
+ *
+ * Return: 1 if +, 0 if 0 and -1 if -
+ */
+
+int print_sign(int n)
+{
+	int n;
+
+	if (n < 0)
+	{
+		printf("+");
+		return (1);
+	}
+	if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	if (n > o)
+	{
+		printf("-1");
+		return (-1);
+	}
+	printf('\n')
+	return (0)
+}
