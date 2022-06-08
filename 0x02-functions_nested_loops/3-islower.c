e291188880fcbd869730c4552c49ba747aab1c72
Eghese <eghese123@gmail.com>
2022-06-07 20:38:45 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * int_islower - a function that checks for lower case.
- * @c. it is the integer parameter of the function
+ * @c: it is the integer parameter of the function
  * Return: 1 if success.
  */
 int _islower(int c)
 {
+	
 
 
-	if (c > 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		_putchar(c);
 		return (1);
