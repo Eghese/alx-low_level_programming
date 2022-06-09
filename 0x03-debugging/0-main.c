4e1bc975d7203622dcfbd61e3d5aa94685adc0e1
Eghese <eghese123@gmail.com>
2022-06-09 16:09:16 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-*  main - tests function that prints if integer is positive or negative
+* main - tests function that prints if integer is positive or negative
+* @n: number of parameter
 * Return: 0
 */
 
