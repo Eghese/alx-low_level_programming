9e48eeb06229e2654123ba310a825b614fa6a79c
Eghese <eghese123@gmail.com>
2022-06-08 21:21:51 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,10 +9,14 @@ void print_to_98(int n)
 {
 	while (n < 98)
 	{
-		printf("%d", n);
-
-		_putchar(',');
+		printf("%d, ", n);
 		n++;
 	}
-	_putchar('\n');
+
+	while  (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	printf("98\n");
 }
