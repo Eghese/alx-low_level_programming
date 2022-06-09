985eeb02a9957ba758c0ce6da677b1f8a1d8f819
Eghese <eghese123@gmail.com>
2022-06-09 16:59:06 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int i;
 
-printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming : \n");
 
 i = 0;
 
@@ -19,7 +19,7 @@ putchar(i);
 i++;
 }
 
-printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\0/\n");
 
 return (0);
 }
