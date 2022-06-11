33d5d8aa200bc32edd87472ea246c4f5d330f118
Eghese <eghese123@gmail.com>
2022-06-10 17:13:42 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - writing a functions that print numbers from 0 to 9
+ * return: 0 if void
+ */
+void print_numbers(void)
+{
+	char a;
+
+	if (a >=48 && a <= 57)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+	
+}
