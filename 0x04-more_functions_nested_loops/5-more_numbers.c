3890c080fa290ffe8c9ab9d4d247eee24024e76b
Eghese <eghese123@gmail.com>
2022-06-10 18:31:25 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - write a functions that print straight line in the code
+ * @n: it is the parameter given
+ * Return: 0 if success
+ */
+void print_line(int n)
+{
+	if (n == 1)
+	{
+		_putchar('_');
+	}
+	if (n == 2)
+	{
+		_putchar('_'+'_');
+
