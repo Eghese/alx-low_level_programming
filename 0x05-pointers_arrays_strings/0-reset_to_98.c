c9844dbc704343ab4eff30dd1af9d410d70eedc6
Eghese <eghese123@gmail.com>
2022-06-13 14:56:48 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * reset_to_98 - writing a function that updates the value of it point
  * @n: parameter of a function.
@@ -6,5 +7,6 @@
 
 void reset_to_98(int *n)
 {
-	int n = 98;
-	int *p = n;
+	int slice = 98;
+	int *p = &slice;
+}
