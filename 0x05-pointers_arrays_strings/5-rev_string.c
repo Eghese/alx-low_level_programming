dc58ecf17e8b495c52078ae36f4b2838efec392b
Eghese <eghese123@gmail.com>
2022-06-14 15:32:49 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,10 +7,23 @@
 void rev_string(char *s)
 {
 	int i = 0;
+	int j = 0;
+	int len = 0;
+	int p = 0;
 
 	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
-		i--;
+		len++;
 	}
+	p = len;
+	while (i < len)
+	{
+		_putchar(s[len - 1]);
+		len--;
+	}
+	if (p > 1)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
 }
