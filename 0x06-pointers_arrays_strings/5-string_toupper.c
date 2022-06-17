3adca72fd248387acd16d89a8fee936f45f1695e
Eghese <eghese123@gmail.com>
2022-06-17 15:55:59 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - converting lower case to uppercase)
+ * @s: a pointer of a char
+ * Return: a pointer of char
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
