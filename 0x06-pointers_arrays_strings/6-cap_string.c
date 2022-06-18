1ba336a292cf9e10588f78edeb8792c8a7059152
Eghese <eghese123@gmail.com>
2022-06-17 20:33:29 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,30 @@
 #include "main.h"
+
 /**
- * cap_string - functions that capitalizes all words.
- * @s: a parameter of char
- * Return: pointer to s
- */
+*cap_string - capitalizes string
+*@s: input string.
+*Return: the pointer to dest.
+*/
 char *cap_string(char *s)
 {
-	int i = 0;
+int count = 0, i;
+int str[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (s[i] != '\0')
-	{
-		if (s[i] >= 'A' && s[i] <= 'Z')
-		{
-			s[i] = s[i] - 32;
-		}
-		i++;
-	}
-	return (s);
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 13; i++)
+{
+if (*(s + count) == str[i])
+{
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
+}
+}
+count++;
+}
+return (s);
 }
