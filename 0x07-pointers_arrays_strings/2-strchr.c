33455477f13e28ccd9eff3a1a6a3ff67b6d3026f
Eghese <eghese123@gmail.com>
2022-06-20 19:43:29 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,12 +3,10 @@
  * _strchr - functions that locates the character of a string
  * @s: a char variable
  * @c: a char variable
- * return: a pointer s or c or null
+ * retur: the value of s
  */
 char *_strchr(char *s, char c)
 {
-	char null[] = "NULL";
-	char *nullguy = null;
 
 	while (*s != '\0')
 	{
@@ -17,10 +15,10 @@ char *_strchr(char *s, char c)
 			return (s);
 			s++;
 		}
+
+		if (*s == c)
+		{
+			return (s);
+		}
 	}
-	if (*s == c)
-	{
-		return (s);
-	}
-	return (nullguy);
 }
