f543f4978afd204f286ef341cc88df44e63920ad
Eghese <eghese123@gmail.com>
2022-06-22 20:04:14 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - functions that prints a strings with a line
+ * @s: a pointer char
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		return;
+	}
+	_putchar('\n');
+	_puts_recursion(s + 1);
+}
