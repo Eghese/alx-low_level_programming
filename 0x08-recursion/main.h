7d9d9d1b022357b6c2f0d9e608eb5671ed68a732
Eghese <eghese123@gmail.com>
2022-06-23 20:19:55 -0700

    6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,3 +5,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _squareRoot(int n, int i);
+int is_prime_number(int n);
+int tmp_prime(int n, int i);
+int divisors(int n, int m);
