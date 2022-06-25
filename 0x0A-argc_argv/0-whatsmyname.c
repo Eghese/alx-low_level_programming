d5a09aaffb754ecd306e12d6a03bce5caa9dfcb9
Eghese <eghese123@gmail.com>
2022-06-24 18:35:29 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,14 @@
 /**
  * main - writing a function that prints its name, followed by a new line.
  * @argv: argument value
+ * @argc:  argument count
  * Return: 0
  */
 int main(char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if(argc >= 0)
+	{	
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }
