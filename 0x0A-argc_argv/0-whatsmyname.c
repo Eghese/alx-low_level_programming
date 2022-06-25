a3ccfd3859a84637ae7ef06ad3c1347170408bd0
Eghese <eghese123@gmail.com>
2022-06-24 17:21:10 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - writing a function that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+	printf("Eghese argv[]\n");
+	return (0);
+}
