53e3ea8a7a43aa7c7bfbee5d9a9294e437dbc01f
Eghese <eghese123@gmail.com>
2022-06-27 19:04:32 -0700

    wow

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
-*  * _atoi - Returns a stirng
-*   * 
-*    * @s - param
-*     * Return: int 
-*      */
-
-
-int _atoi(char *s){
-return (*s);
+ * _atoi - Returns a stirng
+ *@s: param
+ *Return: int
+ */
+int _atoi(char *s)
+{
+	return (*s);
 }
