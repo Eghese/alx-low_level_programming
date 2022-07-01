2b8ed21e00a1ccd8ea9fa392a605b1df8ee3b381
Eghese <eghese123@gmail.com>
2022-06-30 17:59:48 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  _realloc - reallocate memory size function
+ *  @nmemb: unsigned integer
+ *  @size: unsigned integer
+ *  Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+
+	if (new_size > old_size)
+	{
+		s = malloc(new_size)
+		free(ptr);
+		return (s);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
+}
