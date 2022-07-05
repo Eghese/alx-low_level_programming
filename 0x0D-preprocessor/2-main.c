2301b257ff9ca810fea7bee1d502ad5edbd02293
Eghese <eghese123@gmail.com>
2022-07-04 20:26:03 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the name of the file being compiled
+ * Return: success
+ */
+int main(void)
+{
+
+	printf("%s\n", _FILE_);
+	return (0);
+}
