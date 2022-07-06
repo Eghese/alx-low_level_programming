acd25a24afd6970ca1773df68cde4157b5429712
Eghese <eghese123@gmail.com>
2022-07-05 20:15:02 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 /**
  * print_dog - functions that print
  * @d: a pointer to dog
- * Return: void 
+ * Return: void
  */
 void print_dog(struct dog *d)
 {
@@ -14,8 +14,8 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	if (d-> Owner == NULL)
+	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
-		printf("Owner: %s\n", d->Owner);
+		printf("Owner: %s\n", d->owner);
 }
