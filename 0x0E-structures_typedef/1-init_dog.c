c13b5b37b4e429995bef15781c9b8e24294f309d
Eghese <eghese123@gmail.com>
2022-07-05 19:49:45 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,13 +11,15 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	else
 	{
 		return;
 	}
-	else
-		d->name = name;
-		d->age = age;
-	     	d->owner = owner;
 	
 }
