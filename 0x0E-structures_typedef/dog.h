ed5415dd3e2967bee9a9b7917f5589a20268b2cd
Eghese <eghese123@gmail.com>
2022-07-05 18:52:34 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG.H
+#define DOG.H
+
+/**
+* struct dog - a user define data type
+* @name: A char datatype
+* @age: A float datatype
+* @Owner: A char datatype
+* Return: 0 if sucess.
+*/
+typedef struct dog
+{
+	name = char *
+	age = float
+	owner = char *
+} dog_t;
