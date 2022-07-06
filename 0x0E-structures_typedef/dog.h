d34e01209f16aaa0a28d1b8338a57110dda9b763
Eghese <eghese123@gmail.com>
2022-07-05 19:00:53 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,15 +2,17 @@
 #define DOG.H
 
 /**
-* struct dog - a user define data type
-* @name: A char datatype
-* @age: A float datatype
-* @Owner: A char datatype
-* Description: sturcture to store dog
-*/
+ * struct dog - a user define data type
+ * @name: A char datatype
+ * @age: A float datatype
+ * Owner: A char datatype
+ * Return: 0 if sucess.
+ */
 typedef struct dog
 {
-	char *name;
-	float *name;
-	char owner*;
+	char * name;
+	float age;
+	char *owner;
 } dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
