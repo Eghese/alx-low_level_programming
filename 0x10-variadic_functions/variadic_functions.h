35199b17eb36b562e81126db89a4bf0c58226cbb
Eghese <eghese123@gmail.com>
2022-07-07 18:13:42 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -13,7 +13,7 @@ typedef struct pter
 {
 	char *test;
 	void (*printer)();
-}pt;
+} pt;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
